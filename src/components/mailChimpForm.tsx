7841432f8d0f6a44894c9423b29c9da011708062
Marcus <marcus@tryfi.com>
2020-09-09 12:02:21 -0700

    fix mailchimp formik

--- src/components/mailChimpForm.tsx
+++ src/components/mailChimpForm.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import addToMailchimp from "gatsby-plugin-mailchimp"
-import { useFormik } from "formik"
+import { useFormik, Formik } from "formik"
 import { Form, Header, Input, Button, Segment } from 'semantic-ui-react';
 
 import 'semantic-ui-css/semantic.min.css'
@@ -27,21 +27,41 @@ const MailChimpForm = () => {
   return (
     <Segment>
       <header>To receive updates from Marcus consider joining the mailing list</header>
-      <Form>
-        <Form.Field>
-          <small>Name</small>
-          <Input type="text" name="Name" onChange={formik.handleChange}  value={formik.values.firstName} />
-        </Form.Field>
-        <Form.Field>
-          <small>Email</small>
-          <Input type="email" name="Email" onChange={formik.handleChange}  value={formik.values.email} />
-        </Form.Field>
-        <Button
+      <Formik
+      initialValues={{
+        email: "",
+        firstName: "",
+      }}
+      onSubmit={async (values) => {
+        addToMailchimp(values.email, {FNAME: values.firstName})
+      .then(data => {
+        if (data.result === "error") {
+            alert("error: likely a duplicate email");
+        } else {
+          alert("Subscribed")
+        }
+      })
+    }}
+      
+    >
+      {({ values, handleChange}) => (
+        <Form>
+          <Form.Field>
+            <small>Name</small>
+            <Input type="text" name="firstName" onChange={handleChange} value={values.firstName} />
+          </Form.Field>
+          <Form.Field>
+            <small>Email</small>
+            <Input type="email" name="email" onChange={handleChange} value={values.email} />
+          </Form.Field>
+          <Button
           type="submit"
           content="Submit"
           color="blue"
         />
-    </Form>
+        </Form>
+      )}
+    </Formik>
   </Segment>
   )
 }
