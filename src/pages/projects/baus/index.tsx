ae6169a73cd3f7bc712b738eec5ac5a7784e2960
Marcus <marcus@tryfi.com>
2021-06-23 16:11:24 -0700

    baus support app

---
+++ src/pages/projects/baus/index.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { Header } from "semantic-ui-react"
+import Bio from "../../../components/bio"
+import Layout from "../../../components/layout"
+import SEO from "../../../components/seo"
+
+const BausApp = ({location}) => {
+  return (
+    <Layout 
+    location={location} 
+    title={"Marcusmth"}
+    >
+      <SEO title="Baus App" />
+      <Bio/>
+      <Header>Support for Baus App</Header>
+      <p>Contact Marcus on Twitter</p>
+      <a href={`https://twitter.com/marcusmth`}>Twitter</a>
+    </Layout>
+  )
+}
+
+export default BausApp
